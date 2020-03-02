c121c919c352b27fbb7403149c5bd93fcd99fa0a
Tim Chang <tim.chang@mediatek.com>
2020-03-03 00:19:03 +0800

    [ALPS04759130] WMT: resolve Coverity issues
    This change is used to solve Coverity issues.
    
    MTK-Commit-Id: 609cb1fdb3eba382911f2fe1c2b9fb58e57ab174
    
    Change-Id: I810aa1a993455636dfaa0a76e7e9ca3459fc0b15
    CR-Id: ALPS04759130
    Feature: [Module]WMT Driver
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>

--- common_main/core/wmt_ic_6628.c
+++ common_main/core/wmt_ic_6628.c
@@ -1166,8 +1166,8 @@ WMT_CO_CLOCK mt6628_co_clock_get(VOID)
 
 static INT32 mt6628_ver_check(VOID)
 {
-	UINT32 hw_ver;
-	UINT32 fw_ver;
+	UINT32 hw_ver = 0;
+	UINT32 fw_ver = 0;
 	INT32 iret;
 	const WMT_IC_INFO_S *p_info;
 	unsigned long ctrlPa1;
