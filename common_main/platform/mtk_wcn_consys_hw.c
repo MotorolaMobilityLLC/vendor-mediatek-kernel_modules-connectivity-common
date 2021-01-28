e57c6864c7ddeaff57d23b3cdcfe280d5aa763a2
Johnny Yao <johnny.yao@mediatek.com>
2021-01-28 18:12:27 +0800

    [ALPS05277686] wmt: wmt driver porting for mt6833 R
    wmt driver porting for mt6833 R
    
    MTK-Commit-Id: 37dce391fcc239f1ae85907385d4bd0e3c52629a
    
    Change-Id: I752b30b0090cf0375dd8c2bd69577590c1f00871
    Signed-off-by: Johnny Yao <johnny.yao@mediatek.com>
    CR-Id: ALPS05277686
    Feature: [Module]WMT Driver

--- common_main/platform/mtk_wcn_consys_hw.c
+++ common_main/platform/mtk_wcn_consys_hw.c
@@ -130,6 +130,7 @@ const struct of_device_id apwmt_of_ids[] = {
 	{.compatible = "mediatek,mt6779-consys",},
 	{.compatible = "mediatek,mt6768-consys",},
 	{.compatible = "mediatek,mt6785-consys",},
+	{.compatible = "mediatek,mt6833-consys",},
 	{.compatible = "mediatek,mt6853-consys",},
 	{.compatible = "mediatek,mt6873-consys",},
 	{.compatible = "mediatek,mt8168-consys",},
