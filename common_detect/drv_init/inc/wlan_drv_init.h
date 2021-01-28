0a0e85fae516e3442e1a9fbb6e0663b5321e9443
YZ Huang <yz.huang@mediatek.com>
2021-01-28 15:38:00 +0800

    [ALPS03916777] wmt: driver build-in
    [Detail]
    Step 1. remove android.mk in following folder
       vendor/mediatek/kernel_modules/connectivity/common
       vendor/mediatek/kernel_modules/connectivity/wlan/adaptor
       vendor/mediatek/kernel_modules/connectivity/wlan/core/gen4m
    
    Step 2. remove wmt symbol dependency
       vendor/mediatek/kernel_modules/connectivity/bt/mt66xx
         $(BT_EXPORT_SYMBOL).in
       vendor/mediatek/kernel_modules/connectivity/fmradio
         $(FMRADIO_EXPORT_SYMBOL).in
       vendor/mediatek/kernel_modules/connectivity/gps
         $(GPS_EXPORT_SYMBOL).in
    
    Step 3. symlink to the repo path in kernel
    
    Step 4. add build-in option
       # Do build-in for Makefile checking
       export CONFIG_WLAN_DRV_BUILD_IN=y
    
       # Do build-in for xxx.c checking
       subdir-ccflags-y += -D MTK_WCN_REMOVE_KERNEL_MODULE
       subdir-ccflags-y += -D MTK_WCN_BUILT_IN_DRIVER
    
    MTK-Commit-Id: 68510d4e197bfb700851fd3a98cd8e87d22a9d0f
    
    Change-Id: Iacddc4d15c86415acca7f8e0462a83be4ddff95c
    CR-Id: ALPS03916777
    Feature: [Module]WMT Driver
    Signed-off-by: YZ Huang <yz.huang@mediatek.com>

--- common_detect/drv_init/inc/wlan_drv_init.h
+++ common_detect/drv_init/inc/wlan_drv_init.h
@@ -20,16 +20,10 @@ extern int do_wlan_drv_init(int chip_id);
 
 extern int mtk_wcn_wmt_wifi_init(void);
 
-#ifdef MTK_WCN_WLAN_GEN2
 extern int mtk_wcn_wlan_gen2_init(void);
-#endif
 
-#ifdef MTK_WCN_WLAN_GEN3
 extern int mtk_wcn_wlan_gen3_init(void);
-#endif
 
-#ifdef MTK_WCN_WLAN_GEN4
 extern int mtk_wcn_wlan_gen4_init(void);
-#endif
 
 #endif
