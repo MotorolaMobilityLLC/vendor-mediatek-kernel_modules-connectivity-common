5bb56ec1d00c939f7e712a2dae243e97eb93e2b6
longch57 <longch57@motorola.com>
2021-07-22 20:45:15 +0800

    Distinguish different WMT_SOC cfg for milan
    Because there are other projects that share the baseline with Milan，
    so use the values of "androudboot.device" and "andriodboot.radio" to
    distinguish WMT_soc cfg
    
    Change-Id: I85ad21488b2cee4c01f798dc9518068e9700f1a7
    Reviewed-on: https://gerrit.mot.com/2021525
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <longch57@motorola.com>
    Reviewed-by: Qiu Du <duqiu2@motorola.com>
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- common_main/core/wmt_conf.c
+++ common_main/core/wmt_conf.c
@@ -113,6 +113,7 @@ static moto_product products_list[] = {
         {"ellis",    "NCA", "ELLIS"},
         {"ellis",    "CA",  "ELLIS_EPA"},
         {"tonga",    "NA",  "TONGA_EPA"},
+        {"milan",    "all",  "MILAN_FEM"},
 
 
         {{0},        {0},   {0}},
