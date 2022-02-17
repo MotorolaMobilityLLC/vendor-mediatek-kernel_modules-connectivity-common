82147b3b14afabc51d3f7e3e429d0c3c36dea627
Robbin Chiu <robbin.chiu@mediatek.com>
2022-02-17 15:54:53 +0800

    [ALPS05139972] WMT: Fix cfi check fail issue
    [Solution]
    Fix cfi check fail for wmt_plat_deep_idle_ctrl()
    
    MTK-Commit-Id: 52408728f1e3319b0edad0cbea5dd702c1f6d3c6
    
    Change-Id: I5a19bbfe49fdb1d52db6dd0f3c7dfded7dbceb0b
    Signed-off-by: Robbin Chiu <robbin.chiu@mediatek.com>
    CR-Id: ALPS05139972
    Feature: [Module]WMT Driver

--- common_main/linux/include/stp_btif.h
+++ common_main/linux/include/stp_btif.h
@@ -28,7 +28,7 @@ INT32 mtk_wcn_consys_stp_btif_close(VOID);
 INT32 mtk_wcn_consys_stp_btif_rx_cb_register(MTK_WCN_BTIF_RX_CB rx_cb);
 INT32 mtk_wcn_consys_stp_btif_tx(const PUINT8 pBuf, const UINT32 len, PUINT32 written_len);
 INT32 mtk_wcn_consys_stp_btif_wakeup(VOID);
-INT32 mtk_wcn_consys_stp_btif_dpidle_ctrl(enum _ENUM_BTIF_DPIDLE_ en_flag);
+INT32 mtk_wcn_consys_stp_btif_dpidle_ctrl(UINT32 en_flag);
 INT32 mtk_wcn_consys_stp_btif_lpbk_ctrl(enum _ENUM_BTIF_LPBK_MODE_ mode);
 INT32 mtk_wcn_consys_stp_btif_logger_ctrl(enum _ENUM_BTIF_DBG_ID_ flag);
 INT32 mtk_wcn_consys_stp_btif_parser_wmt_evt(const PUINT8 str, UINT32 len);
