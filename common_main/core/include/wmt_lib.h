0401ea264828b2c6b7e8711b22914589e332374d
Danny Shih <danny.shih@mediatek.com>
2023-01-02 21:32:08 +0800

    [ALPS03754929] wmt: wifi on/off move to standalone worker thread
    [Detail]
    WiFi on/off spend too long. It might cause other op command can not be
    handled by wmtd thread in time.
    
    [Solution]
    Move WiFi on/off to standalone worker thread to prevent wmtd thread be
    blocked by WiFi on/off flow.
    
    MTK-Commit-Id: 1835d9b6abd3de0b93353c29e2d3adeddaa16c0f
    
    Change-Id: I7786a9e4f84346fc751b98720b1d4a4665f4d72f
    Feature: [Module]WMT Driver
    Signed-off-by: Danny Shih <danny.shih@mediatek.com>
    CR-Id: ALPS03754929
    (cherry picked from commit 4b093cca38ed135a7aa51b5fffda95cc405cfb5d)

--- common_main/core/include/wmt_lib.h
+++ common_main/core/include/wmt_lib.h
@@ -177,11 +177,14 @@ typedef struct _DEV_WMT_ {
 
 	OSAL_SLEEPABLE_LOCK psm_lock;
 	OSAL_SLEEPABLE_LOCK idc_lock;
+	OSAL_SLEEPABLE_LOCK wlan_lock;
 	/* WMTd thread information */
 	/* struct task_struct *pWmtd;   *//* main thread (wmtd) handle */
 	OSAL_THREAD thread;
 	/* wait_queue_head_t rWmtdWq;  *//*WMTd command wait queue */
 	OSAL_EVENT rWmtdWq;	/* rename */
+	OSAL_THREAD worker_thread;
+	OSAL_EVENT rWmtdWorkerWq;
 	/* ULONG state; *//* bit field of WMT_STAT */
 	OSAL_BIT_OP_VAR state;
 
@@ -213,8 +216,10 @@ typedef struct _DEV_WMT_ {
 
 	OSAL_OP_Q rFreeOpQ;	/* free op queue */
 	OSAL_OP_Q rActiveOpQ;	/* active op queue */
+	OSAL_OP_Q rWorkerOpQ;
 	OSAL_OP arQue[WMT_OP_BUF_SIZE];	/* real op instances */
 	P_OSAL_OP pCurOP;	/* current op */
+	P_OSAL_OP pWorkerOP;	/* current op on worker thread */
 
 	/* cmd str buffer */
 	UINT8 cCmd[NAME_MAX + 1];
@@ -243,6 +248,7 @@ typedef struct _DEV_WMT_ {
 	/* Timer to sync UTC time with connsys */
 	OSAL_TIMER utc_sync_timer;
 	struct work_struct utcSyncWorker;
+	OSAL_TIMER worker_timer;
 } DEV_WMT, *P_DEV_WMT;
 
 
@@ -297,6 +303,7 @@ extern INT32 wmt_lib_register_thermal_ctrl_cb(thermal_query_ctrl_cb thermal_ctrl
 extern P_OSAL_OP wmt_lib_get_free_op(VOID);
 extern INT32 wmt_lib_put_op_to_free_queue(P_OSAL_OP pOp);
 extern MTK_WCN_BOOL wmt_lib_put_act_op(P_OSAL_OP pOp);
+extern MTK_WCN_BOOL wmt_lib_put_worker_op(P_OSAL_OP pOp);
 
 /* extern ENUM_WMTHWVER_TYPE_T wmt_lib_get_hwver (VOID); */
 extern UINT32 wmt_lib_get_icinfo(ENUM_WMT_CHIPINFO_TYPE_T type);
@@ -342,6 +349,8 @@ extern VOID wmt_lib_psm_lock_release(VOID);
 extern INT32 wmt_lib_psm_lock_aquire(VOID);
 extern VOID wmt_lib_idc_lock_release(VOID);
 extern INT32 wmt_lib_idc_lock_aquire(VOID);
+extern VOID wmt_lib_wlan_lock_release(VOID);
+extern INT32 wmt_lib_wlan_lock_aquire(VOID);
 extern INT32 wmt_lib_set_stp_wmt_last_close(UINT32 value);
 
 extern VOID wmt_lib_set_patch_num(UINT32 num);
@@ -351,6 +360,8 @@ extern MTK_WCN_BOOL wmt_lib_stp_is_btif_fullset_mode(VOID);
 
 extern INT32 wmt_lib_set_current_op(P_DEV_WMT pWmtDev, P_OSAL_OP pOp);
 extern P_OSAL_OP wmt_lib_get_current_op(P_DEV_WMT pWmtDev);
+extern INT32 wmt_lib_set_worker_op(P_DEV_WMT pWmtDev, P_OSAL_OP pOp);
+extern P_OSAL_OP wmt_lib_get_worker_op(P_DEV_WMT pWmtDev);
 extern PUINT8 wmt_lib_get_fwinfor_from_emi(UINT8 section, UINT32 offset, PUINT8 buff, UINT32 len);
 extern INT32 wmt_lib_merge_if_flag_ctrl(UINT32 enable);
 extern INT32 wmt_lib_merge_if_flag_get(UINT32 enable);
