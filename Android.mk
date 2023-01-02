bc380638001362e1183b1dae4cd7a2be0cd8617d
Johnny Yao <johnny.yao@mediatek.com>
2023-01-02 22:12:26 +0800

    [ALPS05662039] wmt: wmt driver for layer decoupling 2.0
    wmt driver for layer decoupling 2.0
    
    MTK-Commit-Id: 605a660a309e11c746e5895bbf05fd5d93f1af33
    
    Change-Id: I40862fb0fe6abc2536463d5452f67cc445bbf9f1
    Signed-off-by: Johnny Yao <johnny.yao@mediatek.com>
    CR-Id: ALPS05662039
    Feature: [Module]WMT Driver

--- Android.mk
+++ Android.mk
@@ -1,12 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter yes,$(sort $(MTK_WLAN_SUPPORT) $(MTK_BT_SUPPORT) $(MTK_GPS_SUPPORT) $(MTK_FM_SUPPORT))),)
-WLAN_MT76XX_CHIPS := MT7668 MT7663
-
-ifeq ($(filter $(WLAN_MT76XX_CHIPS), $(MTK_COMBO_CHIP)),)
-ifneq (true,$(strip $(TARGET_NO_KERNEL)))
-ifneq ($(filter yes,$(MTK_COMBO_SUPPORT)),)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := wmt_drv.ko
 LOCAL_PROPRIETARY_MODULE := true
@@ -23,15 +16,3 @@ WMT_OPTS += MTK_PLATFORM_WMT=$(MTK_PLATFORM)
 WMT_OPTS += TARGET_BOARD_PLATFORM_WMT=$(TARGET_BOARD_PLATFORM)
 
 $(linked_module): OPTS += $(WMT_OPTS)
-
-else
-        $(warning wmt_drv-MTK_COMBO_SUPPORT: [$(MTK_COMBO_SUPPORT)])
-endif
-endif
-
-else
-# MT76XX
-$(warning skip wmt_drv)
-$(warning wmt_drv-MTK_COMBO_CHIP: [$(MTK_COMBO_CHIP)])
-endif
-endif
