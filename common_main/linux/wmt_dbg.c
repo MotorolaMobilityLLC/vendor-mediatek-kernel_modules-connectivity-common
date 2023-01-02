ba9a2e8432932a4e6a6f6a930255fc785531f94e
Danny Shih <danny.shih@mediatek.com>
2023-01-02 21:44:57 +0800

    [ALPS03933548] wmt: Add CONFIG_MTK_CONNSYS_DEDICATED_LOG_PATH
    [Detail]
    Some project doe not support the connsys debug utility, but in the chip
    reset process, it calls the  API of the debug utility. Since the
    spinlock did not initialize to cause KE problem.
    
    [Solution]
    Use CONFIG_MTK_CONNSYS_DEDICATED_LOG_PATH to wrap up.
    
    MTK-Commit-Id: b5590a82824cd31b7845fefbe9918e25a2c9d081
    
    Change-Id: I0a22a4bc98cadd967d58f07771169dc0ff7e4f75
    Feature: [Module]WMT Driver
    Signed-off-by: Danny Shih <danny.shih@mediatek.com>
    CR-Id: ALPS03933548

--- common_main/linux/wmt_dbg.c
+++ common_main/linux/wmt_dbg.c
@@ -29,7 +29,9 @@
 #include "psm_core.h"
 #include "stp_core.h"
 #include "stp_dbg.h"
+#ifdef CONFIG_MTK_CONNSYS_DEDICATED_LOG_PATH
 #include "connsys_debug_utility.h"
+#endif
 #include "wmt_step.h"
 #ifdef CONFIG_MTK_ENG_BUILD
 #include "wmt_step_test.h"
@@ -110,8 +112,10 @@ static INT32 wmt_dbg_stp_sdio_reg_read(INT32 par1, INT32 address, INT32 value);
 static INT32 wmt_dbg_stp_sdio_reg_write(INT32 par1, INT32 address, INT32 value);
 static INT32 wmt_dbg_show_thread_debug_info(INT32 par1, INT32 address, INT32 value);
 static INT32 wmt_dbg_met_ctrl(INT32 par1, INT32 met_ctrl, INT32 log_ctrl);
+#ifdef CONFIG_MTK_CONNSYS_DEDICATED_LOG_PATH
 static INT32 wmt_dbg_set_fw_log_mode(INT32 par1, INT32 par2, INT32 par3);
 static INT32 wmt_dbg_emi_dump(INT32 par1, INT32 offset, INT32 size);
+#endif
 static INT32 wmt_dbg_fw_log_ctrl(INT32 par1, INT32 onoff, INT32 level);
 static INT32 wmt_dbg_pre_pwr_on_ctrl(INT32 par1, INT32 enable, INT32 par3);
 #ifdef CONFIG_MTK_ENG_BUILD
@@ -173,8 +177,10 @@ static const WMT_DEV_DBG_FUNC wmt_dev_dbg_func[] = {
 	[0x29] = wmt_dbg_thermal_query,
 	[0x2a] = wmt_dbg_thermal_ctrl,
 	[0x2b] = wmt_dbg_step_ctrl,
+#ifdef CONFIG_MTK_CONNSYS_DEDICATED_LOG_PATH
 	[0x2c] = wmt_dbg_set_fw_log_mode,
 	[0x2d] = wmt_dbg_emi_dump,
+#endif
 	[0x30] = wmt_dbg_show_thread_debug_info,
 #ifdef CONFIG_MTK_ENG_BUILD
 	[0xa0] = wmt_dbg_step_test,
@@ -681,6 +687,7 @@ static INT32 wmt_dbg_ap_reg_write(INT32 par1, INT32 par2, INT32 par3)
 	return 0;
 }
 
+#ifdef CONFIG_MTK_CONNSYS_DEDICATED_LOG_PATH
 static INT32 wmt_dbg_set_fw_log_mode(INT32 par1, INT32 par2, INT32 par3)
 {
 	connsys_dedicated_log_set_log_mode(par2);
@@ -692,6 +699,7 @@ static INT32 wmt_dbg_emi_dump(INT32 par1, INT32 offset, INT32 size)
 	connsys_dedicated_log_dump_emi(offset, size);
 	return 0;
 }
+#endif
 
 #ifdef CONFIG_TRACING
 static INT32 wmt_dbg_ftrace_dbg_log_ctrl(INT32 par1, INT32 par2, INT32 par3)
