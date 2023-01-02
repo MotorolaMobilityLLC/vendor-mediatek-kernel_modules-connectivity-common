caa6810a1f21c866b6cdc67c1ee595f15a1f8a01
Tim Chang <tim.chang@mediatek.com>
2023-01-02 21:57:53 +0800

    [ALPS04294481] WMT: revise mtk_wmt_resume()
    mtk_wmt_resume() might access connsys registers while connsys chip
    is powered on/off. To avoid DEVAPC violation, execute its job in
    wmtd thread.
    
    MTK-Commit-Id: 45ef2524c7c5dda8211c1c06e580c2fb309e7cf9
    
    Change-Id: I1b1398e3c7a35f1f3828b5a86014b48b86193201
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS04294481
    Feature: [Module]WMT Driver

--- common_main/core/include/wmt_lib.h
+++ common_main/core/include/wmt_lib.h
@@ -470,6 +470,7 @@ extern INT32 wmt_lib_get_need_update_patch_version(VOID);
 extern INT32 wmt_lib_set_need_update_patch_version(INT32 need);
 extern VOID wmt_lib_set_bt_link_status(INT32 type, INT32 value);
 VOID mtk_lib_set_mcif_mpu_protection(MTK_WCN_BOOL enable);
+INT32 wmt_lib_resume_dump_info(VOID);
 /*******************************************************************************
 *                              F U N C T I O N S
 ********************************************************************************
