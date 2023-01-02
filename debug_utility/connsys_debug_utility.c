eec7c6f573ee05e4e20c73c4cd338cb013ad8619
Tim Chang <tim.chang@mediatek.com>
2023-01-02 22:11:49 +0800

    [ALPS05551605] WMT: sync patch for common kernel
    Sync patch to Android S for common kernel
    
    MTK-Commit-Id: 3dc5bf0bb4d147634b422c34e040b279c3a06853
    
    Change-Id: If9564a53a62ca04ed15d2cf7440be5ee940c786c
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS05551605
    Feature: [Module]WMT Driver

--- debug_utility/connsys_debug_utility.c
+++ debug_utility/connsys_debug_utility.c
@@ -685,7 +685,7 @@ static enum alarmtimer_restart alarm_timer_handler(struct alarm *alarm,
 	unsigned int tsec, tusec;
 
 	connsys_dedicated_log_get_utc_time(&tsec, &tusec);
-	rtc_time_to_tm(tsec, &tm);
+	rtc_time64_to_tm(tsec, &tm);
 	pr_info("[connsys_log_alarm] alarm_timer triggered [%d-%02d-%02d %02d:%02d:%02d.%09u]"
 			, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday
 			, tm.tm_hour, tm.tm_min, tm.tm_sec, tusec);
@@ -880,7 +880,7 @@ static int connlog_emi_init(phys_addr_t emi_base, const struct connlog_emi_confi
 	}
 
 	gDev.phyAddrEmiBase = emi_base;
-	gDev.virAddrEmiLogBase = ioremap_nocache(gDev.phyAddrEmiBase +
+	gDev.virAddrEmiLogBase = ioremap(gDev.phyAddrEmiBase +
 		emi_config->emi_offset, emi_config->emi_size_total);
 	if (gDev.virAddrEmiLogBase) {
 		pr_info("EMI mapping OK virtual(0x%p) physical(0x%x)\n",
