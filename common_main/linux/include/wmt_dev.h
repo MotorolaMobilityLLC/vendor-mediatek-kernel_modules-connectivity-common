1673e284eb31defa55ed6d1770b44fd3ff4e5cf3
Johnny Yao <johnny.yao@mediatek.com>
2023-01-02 21:52:11 +0800

    [ALPS04139011] wmt: dump information via proc node
    Create proc node, /proc/driver/wmt_dump_info and dump wmt info
    
    Input:
    adb shell cat /proc/driver/wmt_dump_info
    
    Output:
    0x0001ed06;0x0001ed06;0x0001ed06;0x0001ed06;0x0001ed06;0x0000104a
    [CPUPCR_1;CPUPCR_2;CPUPCR_3;CPUPCR_4;CPUPCR_5;LP_WMT_DUMP_INFO]
    
    If connsys reg cannot be read, the output will be 0.
    
    MTK-Commit-Id: 18095329e395049cb56673c7a9736c7c62ea474b
    
    Change-Id: If8b7799eada26b22feeb8a218907ce8f93f6d316
    Signed-off-by: Johnny Yao <johnny.yao@mediatek.com>
    CR-Id: ALPS04139011
    Feature: [Module]WMT Driver

--- common_main/linux/include/wmt_dev.h
+++ common_main/linux/include/wmt_dev.h
@@ -22,6 +22,7 @@
 #define STP_SDIO      0x04
 
 #define CFG_WMT_PROC_FOR_AEE 1
+#define CFG_WMT_PROC_FOR_DUMP_INFO 1
 
 VOID wmt_dev_rx_event_cb(VOID);
 INT32 wmt_dev_rx_timeout(P_OSAL_EVENT pEvent);
