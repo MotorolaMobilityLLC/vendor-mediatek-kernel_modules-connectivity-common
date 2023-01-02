28a48426a5624f239fecd543114b8b6184ee1d87
Danny Shih <danny.shih@mediatek.com>
2023-01-02 21:43:54 +0800

    [ALPS03906515] wmt: Connsys Debug Utility - file rename and code refine
    [Detail]
    Ring and ring_cache are easy to confuse. ring is a ring buffer customized
    for EMI. ring_cache is a generic ring buffer.
    
    [Solution]
    1. Change ring to ring_emi and ring_cache to ring to avoid confusion.
    2. Modify some of the code alignment issues.
    3. Do not pass back the trylocks return value, which will cause the
       function to return to the failed value.
    
    MTK-Commit-Id: d2b1a60529b588db7f92508a43504c4944f03d5c
    
    Change-Id: I20139b032a4f7f910664c9b7d8daca32fa00de94
    Feature: [Module]WMT Driver
    Signed-off-by: Danny Shih <danny.shih@mediatek.com>
    CR-Id: ALPS03906515

--- debug_utility/ring.h
+++ debug_utility/ring.h
@@ -1,18 +1,13 @@
 #ifndef _RING_H_
 #define _RING_H_
 
-#include <linux/io.h>
-#define ROUND_REPEAT
-#define EMI_READ32(addr) (readl(addr))
-#define EMI_WRITE32(addr, data) (writel(data, addr))
-
 struct ring {
 	/* addr where ring buffer starts */
 	void *base;
 	/* addr storing the next writable pos, guaranteed to be >= read except when write overflow, but it's ok. */
-	void *write;
+	unsigned int write;
 	/* addr storing the next readable pos, except when read == write as buffer empty */
-	void *read;
+	unsigned int read;
 	/* must be power of 2 */
 	unsigned int max_size;
 };
@@ -28,43 +23,60 @@ struct ring_segment {
 	unsigned int remain;
 };
 
-void ring_init(void *base, unsigned int max_size, void *read, void *write, struct ring *ring);
+void ring_init(void *base, unsigned int max_size, unsigned int read,
+	unsigned int write, struct ring *ring);
 unsigned int ring_read_prepare(unsigned int sz, struct ring_segment *seg, struct ring *ring);
 #define ring_read_all_prepare(seg, ring)  ring_read_prepare((ring)->max_size, seg, ring)
 unsigned int ring_write_prepare(unsigned int sz, struct ring_segment *seg, struct ring *ring);
+unsigned int ring_overwrite_prepare(unsigned int sz,
+	struct ring_segment *seg, struct ring *ring);
 
 /* making sure max_size is power of 2 */
 #define RING_VALIDATE_SIZE(max_size) WARN_ON(!max_size || (max_size & (max_size - 1)))
 
-#define RING_EMPTY(ring) (EMI_READ32((ring)->read) == EMI_READ32((ring)->write))
+#define RING_EMPTY(ring) ((ring)->read == (ring)->write)
 /* equation works even when write overflow */
-#define RING_SIZE(ring) (EMI_READ32((ring)->write) - EMI_READ32((ring)->read))
-#ifdef ROUND_REPEAT
-#define RING_FULL(ring) (((EMI_READ32((ring)->write) + 1) & ((ring)->max_size - 1)) == EMI_READ32((ring)->read))
-#else
+#define RING_SIZE(ring) ((ring)->write - (ring)->read)
 #define RING_FULL(ring) (RING_SIZE(ring) == (ring)->max_size)
-#endif
+#define RING_WRITE_REMAIN_SIZE(ring) ((ring)->max_size - RING_SIZE(ring))
 
 #define RING_READ_FOR_EACH(_sz, _seg, _ring) \
-	for (_ring_segment_prepare(EMI_READ32((_ring)->read), &(_seg), (_ring)); \
+	for (ring_read_prepare(_sz, &(_seg), _ring), \
+		_ring_segment_prepare((_ring)->read, &(_seg), (_ring)); \
 		(_seg).sz > 0; \
-		_ring_read_commit(&(_seg), (_ring)), \
-		_ring_segment_prepare(EMI_READ32((_ring)->read), &(_seg), (_ring)))
+		_ring_read_commit(&(_seg), (_ring)), _ring_segment_prepare((_ring)->read, \
+			&(_seg), (_ring)))
 
 #define RING_READ_ALL_FOR_EACH(seg, ring) RING_READ_FOR_EACH((ring)->max_size, seg, ring)
 
-#define RING_WRITE_FOR_EACH(_sz, _seg, _ring) \
-	for (_ring_segment_prepare(EMI_READ32((_ring)->write), &(_seg), (_ring)); \
+#define RING_READ_FOR_EACH_ITEM(_sz, _seg, _ring) \
+	for (ring_read_prepare(_sz, &(_seg), _ring), \
+		_ring_segment_prepare_item((_ring)->read, &(_seg), (_ring)); \
 		(_seg).sz > 0; \
-		_ring_write_commit(&(_seg), (_ring)), \
-		_ring_segment_prepare(EMI_READ32((_ring)->write), &(_seg), (_ring)))
+		_ring_read_commit(&(_seg), (_ring)), _ring_segment_prepare_item((_ring)->read, \
+			&(_seg), (_ring)))
+
+#define RING_WRITE_FOR_EACH(_sz, _seg, _ring) \
+	for (ring_write_prepare(_sz, &(_seg), _ring),\
+		_ring_segment_prepare((_ring)->write, &(_seg), (_ring)); \
+		(_seg).sz > 0; \
+		_ring_write_commit(&(_seg), (_ring)), _ring_segment_prepare((_ring)->write, \
+			&(_seg), (_ring)))
+
+#define RING_OVERWRITE_FOR_EACH(_sz, _seg, _ring) \
+	for (ring_overwrite_prepare(_sz, &(_seg), _ring), \
+		_ring_segment_prepare((_ring)->write, &(_seg), (_ring)); \
+		(_seg).sz > 0; \
+		_ring_write_commit(&(_seg), (_ring)), _ring_segment_prepare((_ring)->write, \
+			&(_seg), (_ring)))
 
 void ring_dump(const char *title, struct ring *ring);
 void ring_dump_segment(const char *title, struct ring_segment *seg);
 
 
-/* Ring Buffer Internal API */
+/* ring Buffer Internal API */
 void _ring_segment_prepare(unsigned int from, struct ring_segment *seg, struct ring *ring);
+void _ring_segment_prepare_item(unsigned int from, struct ring_segment *seg, struct ring *ring);
 void _ring_read_commit(struct ring_segment *seg, struct ring *ring);
 void _ring_write_commit(struct ring_segment *seg, struct ring *ring);
 
