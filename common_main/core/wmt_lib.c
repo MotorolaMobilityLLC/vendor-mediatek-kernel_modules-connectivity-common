e9b3bc5731505c5bb7b74bcc9c2903f9d6a6a760
Danny Shih <danny.shih@mediatek.com>
2023-01-02 21:38:04 +0800

    [ALPS03758079] wmt: Dump connsys EMI data
    [Detail]
    Half of Coredump data comes from connsys EMI. Direct storage of
    these data through the driver cat save nearly half of coredump
    time.
    
    [Solution]
    1. stp_dump directly stores connsys EMI through the  mmap method.
    2. Set 30 seconds timer for stp_dump to dump connsys EMI.
    3. In AEE mode, the connsys EMI dump is named SYS_WCN_EMI_DUMP,
       through AEE copy to db. In stp_dump mode, it has the same name
       as coredump file, but the file extension is .emi.
       Ex: combo_t32_20100103122831.cmm
           combo_t32_20100103122831.emi
    
    MTK-Commit-Id: baa8ae0865b20fbe5ba87f830e8248db55f36c73
    
    Change-Id: Idba639c9c6c573ceb9e9f43999274dc3e540bb5f
    Feature: [Module]WMT Driver
    Signed-off-by: Danny Shih <danny.shih@mediatek.com>
    CR-Id: ALPS03758079
    (cherry picked from commit 649bc5f0c6ed57c4093bef75a3eaf8d464a29c37)

--- common_main/core/wmt_lib.c
+++ common_main/core/wmt_lib.c
@@ -2184,6 +2184,7 @@ ENUM_WMTRSTRET_TYPE_T wmt_lib_cmb_rst(ENUM_WMTRSTSRC_TYPE_T src)
 	}
 	mtk_wcn_stp_coredump_start_ctrl(0);
 	mtk_wcn_stp_set_wmt_trg_assert(0);
+	mtk_wcn_stp_emi_dump_flag_ctrl(0);
 rstDone:
 	osal_clear_bit(WMT_STAT_RST_ON, &pDevWmt->state);
 	return retval;
