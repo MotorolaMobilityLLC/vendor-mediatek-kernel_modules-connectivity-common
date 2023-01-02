36d11c8689f4ecf3a2ec672dabd0a12c1382a9c1
can.chen <can.chen@mediatek.com>
2023-01-02 21:48:47 +0800

    [ALPS02870048] WMT: Fix build error
    [Description]
    wmt_drv.ko is not need by mt7668
    
    MTK-Commit-Id: 908a56cadb4f8174348781b384e547801dd38a4e
    
    Change-Id: I6057e7ede4a7b558c14840d7195a99bf7400d2dc
    Signed-off-by: can.chen <can.chen@mediatek.com>
    Feature: [Module]Kernel Maintenance
    CR-Id: ALPS02870048

--- Android.mk
+++ Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter yes,$(sort $(MTK_WLAN_SUPPORT) $(MTK_BT_SUPPORT) $(MTK_GPS_SUPPORT) $(MTK_FM_SUPPORT))),)
 
+ifneq ($(MTK_COMBO_CHIP),MT7668)
+
 ifneq (true,$(strip $(TARGET_NO_KERNEL)))
 ifneq ($(filter yes,$(MTK_COMBO_SUPPORT)),)
 
@@ -19,6 +21,9 @@ include $(MTK_KERNEL_MODULE)
 else
         $(warning wmt_drv-MTK_COMBO_SUPPORT: [$(MTK_COMBO_SUPPORT)])
 endif
+endif
+else
+        $(warning skip wmt_drv for $(MTK_COMBO_CHIP))
 endif
 
 endif
