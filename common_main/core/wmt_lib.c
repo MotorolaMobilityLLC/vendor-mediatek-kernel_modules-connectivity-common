c3be8ba72cdcb6b15f1f3e6f0996ac2bec475c7c
Tim Chang <tim.chang@mediatek.com>
2023-05-24 11:47:24 +0800

    [ALPS07900748] wmt: revise condition of printing debug log
    allow bus log to be printed during connsys power-on process.
    
    MTK-Commit-Id: 04a690ed1ed3f4c174bf529920233581c9216f24
    
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS07900748
    Feature: wmt
    Change-Id: I8a3285a64a9e91ed52a3c00bccbba89a8cf27384

--- common_main/core/wmt_lib.c
+++ common_main/core/wmt_lib.c
@@ -3477,18 +3477,8 @@ INT32 wmt_lib_cmd_pre_rx_timeout_dump(VOID)
 {
 	int ret;
 
-	ret = wmt_lib_power_lock_aquire();
-	if (ret != 0) {
-		WMT_ERR_FUNC("aquire power lock fail ret=[%d]", ret);
-		return -1;
-	}
-	if (wmt_lib_get_drv_status(WMTDRV_TYPE_WMT) == DRV_STS_POWER_OFF) {
-		wmt_lib_power_lock_release();
-		return 0;
-	}
 	WMT_INFO_FUNC("======================== ");
 	ret = mtk_wcn_consys_cmd_pre_rx_timeout_dump();
-	wmt_lib_power_lock_release();
 
 	return ret;
 }
