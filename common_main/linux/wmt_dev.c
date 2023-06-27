b2da81962b44ead53d983f72aff2b9a0382b8476
Robbin Chiu <robbin.chiu@mediatek.com>
2023-06-27 09:05:08 +0800

    [ALPS08158734] wmt: fix build error
    Use vm flag clear API in kernel 6.1
    
    MTK-Commit-Id: 497f2b78c7bd5e89da3a9bf1b7e0e21a445e4d28
    
    Signed-off-by: Robbin Chiu <robbin.chiu@mediatek.com>
    CR-Id: ALPS08158734
    Feature: wmt
    Change-Id: Iaee24fc546644577be2c98bd11cdbd12a646dc81

--- common_main/linux/wmt_dev.c
+++ common_main/linux/wmt_dev.c
@@ -1532,7 +1532,11 @@ static INT32 WMT_mmap(struct file *pFile, struct vm_area_struct *pVma)
 	unsigned long bufId = pVma->vm_pgoff;
 	P_CONSYS_EMI_ADDR_INFO emiInfo = mtk_wcn_consys_soc_get_emi_phy_add();
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
+	vm_flags_clear(pVma, VM_WRITE | VM_MAYWRITE);
+#else
 	pVma->vm_flags &= ~(VM_WRITE | VM_MAYWRITE);
+#endif
 	WMT_INFO_FUNC("WMT_mmap start:%lu end:%lu size: %lu buffer id=%lu\n",
 		pVma->vm_start, pVma->vm_end,
 		pVma->vm_end - pVma->vm_start, bufId);
