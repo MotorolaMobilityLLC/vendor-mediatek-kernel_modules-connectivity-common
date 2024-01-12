f2aed9452272940c530abf60cce2c75a8d3bad08
Tim Chang <tim.chang@mediatek.com>
2024-01-12 20:14:34 +0800

    [ALPS05886899] wmt: set driving of TOP/BT to 4 mA
    Set driving of TOP/BT to 4 mA
    
    MTK-Commit-Id: 00667407096640920b0e4fdc39584433245eaa4e
    
    Change-Id: I17c87c5208f18161566ad120ce28d5c942335afe
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS05886899
    Feature: [Module]WMT Driver

--- common_main/platform/mt6768.c
+++ common_main/platform/mt6768.c
@@ -515,6 +515,7 @@ static INT32 consys_clock_buffer_ctrl(MTK_WCN_BOOL enable)
 static VOID consys_set_if_pinmux(MTK_WCN_BOOL enable)
 {
 	UINT8 *consys_if_pinmux_reg_base = NULL;
+	UINT8 *consys_if_driving_reg_base = NULL;
 
 	/* Switch D die pinmux for connecting A die */
 	consys_if_pinmux_reg_base = ioremap_nocache(CONSYS_IF_PINMUX_REG_BASE, 0x1000);
@@ -530,6 +531,20 @@ static VOID consys_set_if_pinmux(MTK_WCN_BOOL enable)
 		CONSYS_REG_WRITE(consys_if_pinmux_reg_base + CONSYS_IF_PINMUX_02_OFFSET,
 				(CONSYS_REG_READ(consys_if_pinmux_reg_base + CONSYS_IF_PINMUX_02_OFFSET) &
 				CONSYS_IF_PINMUX_02_MASK) | CONSYS_IF_PINMUX_02_VALUE);
+
+		consys_if_driving_reg_base = ioremap_nocache(CONSYS_IF_DRV_PINMUX_REG_BASE, 0x10);
+		if (!consys_if_driving_reg_base) {
+			iounmap(consys_if_pinmux_reg_base);
+			WMT_PLAT_PR_INFO("consys_if_driving_reg_base(%x) ioremap fail\n",
+				CONSYS_IF_DRV_PINMUX_REG_BASE);
+			return;
+		}
+		CONSYS_REG_WRITE(consys_if_driving_reg_base,
+				(CONSYS_REG_READ(consys_if_driving_reg_base) & CONSYS_IF_DRV_PINMUX_MASK) |
+				CONSYS_IF_DRV_PINMUX_VALUE);
+
+		iounmap(consys_if_driving_reg_base);
+
 	} else {
 		CONSYS_REG_WRITE(consys_if_pinmux_reg_base + CONSYS_IF_PINMUX_01_OFFSET,
 				CONSYS_REG_READ(consys_if_pinmux_reg_base + CONSYS_IF_PINMUX_01_OFFSET) &
