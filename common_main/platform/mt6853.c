0d12de385a7412433e8ddb218eb97e4eb6f17334
Tim Chang <tim.chang@mediatek.com>
2024-04-26 21:03:26 +0800

    [ALPS08743230] wmt: mt6853: revise EMI MPU API
    [Description]
    Revise for kernel migration.
    
    [Test]
    Build pass
    
    MTK-Commit-Id: 11ef8191a7b930d2c0b0dc82902e43241ba1c76e
    
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS08743230
    Feature: wmt
    Change-Id: Ie17dfa6f500230cf64060c993605e6f950726b97

--- common_main/platform/mt6853.c
+++ common_main/platform/mt6853.c
@@ -18,7 +18,6 @@
 #endif
 #define DFT_TAG "[WMT-CONSYS-HW]"
 
-#define CONSYS_ENABLE_EMI_MPU 1
 #define	REGION_CONN	27
 
 #define	DOMAIN_AP	0
@@ -52,10 +51,16 @@
 #include "wmt_plat.h"
 #include "stp_dbg.h"
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
-#include <soc/mediatek/emi.h>
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0))
+#if IS_ENABLED(CONFIG_MTK_EMI_LEGACY)
+#define CONSYS_ENABLE_EMI_MPU 1
+#include "soc/mediatek/emi.h"
+#endif
 #else
-#include <memory/mediatek/emi.h>
+#if IS_ENABLED(CONFIG_MTK_EMI)
+#define CONSYS_ENABLE_EMI_MPU 1
+#include <mt_emi_api.h>
+#endif
 #endif
 
 #if CONSYS_PMIC_CTRL_ENABLE
@@ -2056,7 +2061,7 @@ static INT32 consys_hw_vcn_ctrl_after_idle(VOID)
 
 static INT32 consys_emi_mpu_set_region_protection(VOID)
 {
-#if CONSYS_ENABLE_EMI_MPU
+#ifdef CONSYS_ENABLE_EMI_MPU
 	struct emimpu_region_t region;
 	unsigned long long start = gConEmiPhyBase;
 	unsigned long long end = gConEmiPhyBase + gConEmiSize - 1;
