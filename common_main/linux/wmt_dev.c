76352a2c25f0278cd8d10836e87f5e050b686902
Tim Chang <tim.chang@mediatek.com>
2024-05-28 10:19:42 +0800

    [ALPS08976634] wmt: update DRM option
    [Description]
    Use CONFIG_DEVICE_MODULES_DRM_MEDIATEK to check whether
    mtk_disp_notifier_register is supported.
    
    [Test]
    Build pass
    
    MTK-Commit-Id: ba014cf3aeff576287205014ff84b80d951bf06b
    
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS08976634
    Feature: wmt
    Change-Id: I19439a5a3fc3b283beff32164c1a8a0f821f15c8

--- common_main/linux/wmt_dev.c
+++ common_main/linux/wmt_dev.c
@@ -1742,7 +1742,7 @@ static INT32 WMT_init(VOID)
 #else
 	wmt_fb_notifier.notifier_call = wmt_fb_notifier_callback;
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0))
-#if IS_ENABLED(CONFIG_DRM_MEDIATEK)
+#if IS_ENABLED(CONFIG_DEVICE_MODULES_DRM_MEDIATEK)
 	ret = mtk_disp_notifier_register("wmt_driver", &wmt_fb_notifier);
 #endif
 #else
@@ -1807,7 +1807,7 @@ static VOID WMT_exit(VOID)
 	WMT_INFO_FUNC("unregister_early_suspend finished\n");
 #else
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0))
-#if IS_ENABLED(CONFIG_DRM_MEDIATEK)
+#if IS_ENABLED(CONFIG_DEVICE_MODULES_DRM_MEDIATEK)
 	mtk_disp_notifier_unregister(&wmt_fb_notifier);
 #endif
 #else
